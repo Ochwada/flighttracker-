bf916cfda6cb38c71902541ee8aa0a7e176814f2
ochwada <nakhulolinda@yahoo.com>
2025-06-10 15:11:59 +0200

    Flight Class class & FlightStatus enum

--- src/main/java/com/flighttracker/App.java
+++ src/main/java/com/flighttracker/App.java
@@ -9,21 +9,38 @@ public class App
 {
     public static void main( String[] args )
     {
-        Flight flight = new Flight(
+        Flight flight1 = new Flight(
                 "XY123",
                 LocalDateTime.of(2025, 6, 9, 17, 0),
                 ZoneId.of("Europe/London")
         );
 
-        Flight flight1 = new Flight(
-                "XY001",
-                LocalDateTime.now().minusHours(1),
-                ZoneId.of("Europe/London")
+        Flight flight2 = new Flight(
+                "LH456",
+                LocalDateTime.of(2025, 6, 10, 9, 30),
+                ZoneId.of("Europe/Berlin")
+        );
+
+        Flight flight3 = new Flight(
+                "EK202",
+                LocalDateTime.of(2025, 6, 11, 23, 45),
+                ZoneId.of("Asia/Dubai")
+        );
+        Flight flight4 = new Flight(
+                "AF102",
+                LocalDateTime.now().minusHours(1), // already departed
+                ZoneId.of("Europe/Paris")
         );
 
 
 
-        flight.printFlightDetails();
+
         flight1.printFlightDetails();
+        System.out.println();
+        flight2.printFlightDetails();
+        System.out.println();
+        flight3.printFlightDetails();
+        System.out.println();
+        flight4.printFlightDetails();
     }
 }
