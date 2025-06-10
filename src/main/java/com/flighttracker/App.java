57a02f60150602e6229c62cb220d1cc91f09a763
ochwada <nakhulolinda@yahoo.com>
2025-06-10 12:38:26 +0200

    Initial project setup

---
+++ src/main/java/com/flighttracker/App.java
@@ -0,0 +1,13 @@
+package com.flighttracker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
